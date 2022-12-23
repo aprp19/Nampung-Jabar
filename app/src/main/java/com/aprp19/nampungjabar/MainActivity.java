3a6d4f73721bdb46eb0366458f5aa74476466df2
Angga Puja Restu Prakasa <kierska19@gmail.com>
2022-12-23 14:58:02 +0700

    Adding top toolbar and bottom navigation

--- app/src/main/java/com/aprp19/nampungjabar/MainActivity.java
+++ app/src/main/java/com/aprp19/nampungjabar/MainActivity.java
@@ -1,8 +1,10 @@
 package com.aprp19.nampungjabar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buat nampilin button di top toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //ini juga buat nampilin toolbar
+        getMenuInflater().inflate(R.menu.top_toolbar, menu);
+        return true;
     }
 }

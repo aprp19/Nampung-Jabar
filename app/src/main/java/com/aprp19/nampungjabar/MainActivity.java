7a956a7e41a5e4806d2b6971b16131651cc4f962
Angga Puja Restu Prakasa <kierska19@gmail.com>
2022-12-25 16:32:51 +0700

    Adding Fragment
    Took 7 hours 47 minutes

--- app/src/main/java/com/aprp19/nampungjabar/MainActivity.java
+++ app/src/main/java/com/aprp19/nampungjabar/MainActivity.java
@@ -1,21 +1,26 @@
 package com.aprp19.nampungjabar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 import com.aprp19.nampungjabar.adapter.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    ViewPager viewPager;
-    WormDotsIndicator dotsIndicator;
-    ViewPagerAdapter viewAdapter;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +31,43 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-        //Buat nampilin View Pager
-        viewPager = findViewById(R.id.vp_container);
-        dotsIndicator = findViewById(R.id.vp_dotsIndicator);
-        viewAdapter = new ViewPagerAdapter(this);
-        viewPager.setAdapter(viewAdapter);
-        //noinspection deprecation
-        dotsIndicator.setViewPager(viewPager);
+        bottomNavigationView = findViewById(R.id.tb_bottomnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.btnav_home);
+
+        FragmentTransaction homeTransaction = getSupportFragmentManager().beginTransaction();
+        homeTransaction.replace(R.id.fg_content, homeFragment,"" );
+        homeTransaction.commit();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) { //ini juga buat nampilin toolbar
         getMenuInflater().inflate(R.menu.top_toolbar, menu);
         return true;
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    ListFragment listFragment = new ListFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.btnav_home) {
+            FragmentTransaction homeTransaction = getSupportFragmentManager().beginTransaction();
+            homeTransaction.replace(R.id.fg_content, homeFragment, "");
+            homeTransaction.commit();
+        } else if (item.getItemId() == R.id.btnav_listview) {
+            FragmentTransaction listTransaction = getSupportFragmentManager().beginTransaction();
+            listTransaction.replace(R.id.fg_content, listFragment, "");
+            listTransaction.commit();
+        } else if (item.getItemId() == R.id.btnav_settings) {
+            FragmentTransaction settingsTransaction = getSupportFragmentManager().beginTransaction();
+            settingsTransaction.replace(R.id.fg_content, settingsFragment, "");
+            settingsTransaction.commit();
+        }
+
+        return false;
     }
 }
